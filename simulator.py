c8f4caa25bdc9edd55e1ac98c484c33242a3e415
Crystoffar <crystoffar@gmail.com>
2020-08-24 22:45:04 -0700

    Created rolling and opening functions

--- simulator.py
+++ simulator.py
@@ -1,3 +1,34 @@
 import random
 
-print("hello world")
+def rollNormal():
+    randNum = random.randrange(10001)
+
+    if randNum < 168:
+        return "RRR"
+
+    if randNum < 668:
+        return "RR"
+
+    if randNum < 1167:
+        return "R"
+
+    return "C"
+
+def rollFixed():
+    randNum = random.randrange(10001)
+
+    if randNum < 168:
+        return "RRR"
+
+    if randNum < 668:
+        return "RR"
+
+    return "R"
+
+def openPack():
+    for i in range(5):
+        print(rollNormal())
+    print(rollFixed())
+    pass
+
+openPack()
