589ddaeb18f203593f6afc62fff8294062553433
Crystoffar <crystoffar@gmail.com>
2020-08-25 13:35:16 -0700

    Created export function

--- simulator.py
+++ simulator.py
@@ -8,6 +8,8 @@ RList = []
 RRList = []
 RRRList = []
 
+exportDict = dict()
+
 def importSet():
     setCSV = open(fileName, "r")
     dataReader = csv.reader(setCSV)
@@ -25,26 +27,36 @@ def importSet():
         if data[row][0] == "RRR":
             RRRList.append(data[row][1])
 
+def addCard(cardName):
+    if cardName in exportDict:
+        exportDict[cardName] += 1
+    else:
+        exportDict[cardName] = 1
+
 def rollNormal():
     randNum = random.randrange(10001)
 
     if randNum < 168:
         cardNum = random.randrange(len(RRRList))
         print("[RRR] " + RRRList[cardNum])
+        addCard(RRRList[cardNum])
         return
 
     if randNum < 668:
         cardNum = random.randrange(len(RRList))
         print("[RR] " + RRList[cardNum])
+        addCard(RRList[cardNum])
         return
 
     if randNum < 1167:
         cardNum = random.randrange(len(RList))
         print("[R] " + RList[cardNum])
+        addCard(RList[cardNum])
         return
 
     cardNum = random.randrange(len(CList))
     print("[C] " + CList[cardNum])
+    addCard(CList[cardNum])
 
 def rollFixed():
     randNum = random.randrange(10001)
@@ -52,15 +64,18 @@ def rollFixed():
     if randNum < 168:
         cardNum = random.randrange(len(RRRList))
         print("[RRR] " + RRRList[cardNum])
+        addCard(RRRList[cardNum])
         return
 
     if randNum < 668:
         cardNum = random.randrange(len(RRList))
         print("[RR] " + RRList[cardNum])
+        addCard(RRList[cardNum])
         return
 
     cardNum = random.randrange(len(RList))
     print("[R] " + RList[cardNum])
+    addCard(RList[cardNum])
 
 def openPack():
     for i in range(5):
@@ -78,5 +93,17 @@ def openBox():
             else:
                 answer = input("Finish? (Y/N) ")
 
+def exportPulls():
+    exportCSV = "pulls.csv"
+    try:
+        with open(exportCSV, 'w') as csvFile:
+            writer = csv.writer(csvFile)
+            for cardName, quantity in exportDict.items():
+                writer.writerow([cardName, quantity])
+    except IOError:
+        print("I/O error")
+
+#Main
 importSet()
 openBox()
+exportPulls()
