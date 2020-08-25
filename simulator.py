617f108efdf2e7b67ba05109c59517c2c2936569
Crystoffar <crystoffar@gmail.com>
2020-08-24 23:56:56 -0700

    Created import function, modified opening packs

--- simulator.py
+++ simulator.py
@@ -1,34 +1,71 @@
 import random
+import csv
+
+fileName = "01_king_of_knights.csv"
+
+CList = []
+RList = []
+RRList = []
+RRRList = []
+
+def importSet():
+    setCSV = open(fileName, "r")
+    dataReader = csv.reader(setCSV)
+    data = []
+    for row in dataReader:
+        data.append(row)
+
+    for row in range(len(data)):
+        if data[row][0] == "C":
+            CList.append(data[row][1])
+        if data[row][0] == "R":
+            RList.append(data[row][1])
+        if data[row][0] == "RR":
+            RRList.append(data[row][1])
+        if data[row][0] == "RRR":
+            RRRList.append(data[row][1])
 
 def rollNormal():
     randNum = random.randrange(10001)
 
     if randNum < 168:
-        return "RRR"
+        cardNum = random.randrange(len(RRRList))
+        print("[RRR] " + RRRList[cardNum])
+        return
 
     if randNum < 668:
-        return "RR"
+        cardNum = random.randrange(len(RRList))
+        print("[RR] " + RRList[cardNum])
+        return
 
     if randNum < 1167:
-        return "R"
+        cardNum = random.randrange(len(RList))
+        print("[R] " + RList[cardNum])
+        return
 
-    return "C"
+    cardNum = random.randrange(len(CList))
+    print("[C] " + CList[cardNum])
 
 def rollFixed():
     randNum = random.randrange(10001)
 
     if randNum < 168:
-        return "RRR"
+        cardNum = random.randrange(len(RRRList))
+        print("[RRR] " + RRRList[cardNum])
+        return
 
     if randNum < 668:
-        return "RR"
+        cardNum = random.randrange(len(RRList))
+        print("[RR] " + RRList[cardNum])
+        return
 
-    return "R"
+    cardNum = random.randrange(len(RList))
+    print("[R] " + RList[cardNum])
 
 def openPack():
     for i in range(5):
-        print(rollNormal())
-    print(rollFixed())
-    pass
+        rollNormal()
+    rollFixed()
 
+importSet()
 openPack()
